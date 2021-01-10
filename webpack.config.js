71b1a0d385a0de037534dd82e203ee2077333e66
kakigakki <kakigakki@yahoo.co.jp>
2021-01-10 22:41:56 +0900

    project init

---
+++ webpack.config.js
@@ -0,0 +1,91 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+    optimization: {
+        minimize: false // 关闭代码压缩，可选
+    },
+
+    entry: "./src/index.ts",
+
+    devtool: "inline-source-map",
+
+    devServer: {
+        contentBase: './dist'
+    },
+
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+        environment: {
+            arrowFunction: false // 关闭webpack的箭头函数，可选
+        }
+    },
+
+    resolve: {
+        extensions: [".ts", ".js"]
+    },
+
+    module: {
+        rules: [{
+                test: /\.ts$/,
+                use: [{
+                        loader: "babel-loader",
+                        options: {
+                            presets: [
+                                [
+                                    "@babel/preset-env",
+                                    {
+                                        "targets": {
+                                            "chrome": "58",
+                                            "ie": "11"
+                                        },
+                                        "corejs": "3",
+                                        "useBuiltIns": "usage"
+                                    }
+                                ]
+                            ]
+                        }
+                    },
+                    {
+                        loader: "ts-loader",
+                    }
+                ],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    [
+                                        // 浏览器兼容插件
+                                        "postcss-preset-env",
+                                        {
+                                            // 每个浏览器最新两个版本
+                                            browsers: 'last 2 versions'
+                                        }
+                                    ]
+                                ]
+                            }
+                        }
+                    },
+                    "less-loader"
+                ]
+            }
+        ]
+    },
+
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        }),
+    ]
+}
